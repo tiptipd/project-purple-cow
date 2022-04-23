11261ee154757ad76e91142a9fb62f1336af701f
tiptipd <jub.han@yahoo.com>
2022-04-23 18:04:39 +0500

    added functionality on the main entry point app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// * Libraries
+import { useEffect, useState } from "react";
+import countapi from "countapi-js";
+
+// * Components
+import Button from "./components/Button";
+import Counter from "./components/Counter";
+
+// * Assets
+import logo from "./logo.svg";
+import "./App.css";
+
+const NAMESPACE = process.env.REACT_APP_NAMESPACE;
+const KEY = process.env.REACT_APP_KEY;
 
 function App() {
+  const [currentCount, setcurrentCount] = useState(0);
+  const [previousCount, setPreviousCount] = useState(0);
+
+  useEffect(() => {
+    getCurrentCount();
+  }, []);
+
+  const getCurrentCount = async () => {
+    await countapi.get(NAMESPACE, KEY).then((result) => {
+      setcurrentCount(result.value.toString());
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo animation" />
+
+        <Counter currentCount={currentCount} previousCount={previousCount} />
+
+        <Button
+          setcurrentCount={setcurrentCount}
+          currentCount={currentCount}
+          setPreviousCount={setPreviousCount}
+        />
       </header>
     </div>
   );
